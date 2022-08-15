143e6fd6b06dbf7df932b06af947776b9a766665
Deepak <deepak.281295@gmail.com>
2022-08-15 22:40:45 +0530

    added longest substring without repeating characters

---
+++ SlidingWindow/LongestSubstringWithoutRepeatingChars.js
@@ -0,0 +1,53 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    
+    // Brute Force O(n^3)
+    // function checkDuplicate(s, i, j) {
+    //     const uniqueChars = new Array(256).fill(false);
+    //     for (let k = i; k <= j; k++) {
+    //         if (uniqueChars[s.charCodeAt(k)]) {
+    //             return true;
+    //         }
+    //         uniqueChars[s.charCodeAt(k)] = true;
+    //     }
+    //     return false;
+    // }
+    // let res = 0;
+    // for (let i = 0; i < s.length; i++) {
+    //     for (let j = i; j < s.length; j++) {
+    //         if (!checkDuplicate(s, i, j)) {
+    //             res = Math.max(res, j - i + 1);
+    //         }
+    //     }
+    // }
+    // return res;
+    
+    // Brute Force O(n^2)
+    // let res = 0;
+    // for (let  i = 0; i < s.length; i++) {
+    //     const validChars = new Array(256).fill(false);
+    //     for (let j = i; j < s.length; j++) {
+    //         if (validChars[s.charCodeAt(j)]) {
+    //             break;
+    //         }
+    //         validChars[s.charCodeAt(j)] = true;
+    //         res = Math.max(res, j - i + 1);
+    //     }
+    // }
+    // return res;
+    
+    // 
+    let res = 0;
+    const prev = new Array(256).fill(-1);
+    let start = 0;
+    
+    for (let end = 0; end < s.length; end++) {
+        start = Math.max(start, prev[s.charCodeAt(end)] + 1);
+        res = Math.max(res, end - start + 1);
+        prev[s.charCodeAt(end)] = end;
+    }
+    return res;
+};
